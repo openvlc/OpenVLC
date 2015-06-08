d50813b860211fe31769c347067fa4a7f80246eb
openvlc <wangqingqw@gmail.com>
2015-06-08 19:28:43 +0200

    Update vlc.h

--- vlc.h
+++ vlc.h
@@ -1,6 +1,20 @@
 /*
- Copyright (c) 2014, IMDEA NETWORKS Institute 
- All rights reserved.
+  Copyright (c) 2014, IMDEA NETWORKS Institute 
+  
+  This file is part of the OpenVLC's source codes.
+  
+  OpenVLC's source codes are free: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  OpenVLC's source codes are distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with the source codes of OpenVLC.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
