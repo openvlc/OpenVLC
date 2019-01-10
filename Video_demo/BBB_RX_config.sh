f6dd862a879c417b9d3e8058ef2c3962fb6136ae
Diego <d22prw@gmail.com>
2019-01-10 13:05:01 +0100

    Update BBB_RX_config.sh

--- Video_demo/BBB_RX_config.sh
+++ Video_demo/BBB_RX_config.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 cd kernel
-sudo ./load_test.sh
+sudo ./load_test.sh #Load OpenVLC driver
 cd ..
 cd PRU/RX_back
-sudo ./deploy.sh
+sudo ./deploy.sh #Load OpenVLC firmware
 cd ..
 cd ..
-echo "1" | sudo tee /proc/sys/net/ipv4/ip_forward
-sudo route add -net 192.168.7.0/24 gw 192.168.6.1
-sudo route add -net 192.168.2.0/24 gw 192.168.6.1
+echo "1" | sudo tee /proc/sys/net/ipv4/ip_forward #Enable forwarding
+sudo route add -net 192.168.7.0/24 gw 192.168.6.1 #Add USB server interface route
+sudo route add -net 192.168.2.0/24 gw 192.168.6.1 #Add ethernet server interface route
 echo "RX configured"
