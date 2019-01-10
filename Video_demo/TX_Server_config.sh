6fdfccf443d6088ee1caf142459eea6b48cb7fe8
Diego <d22prw@gmail.com>
2019-01-10 13:06:03 +0100

    Update TX_Server_config.sh

--- Video_demo/TX_Server_config.sh
+++ Video_demo/TX_Server_config.sh
@@ -1,7 +1,11 @@
-sudo route add -net 192.168.6.0/24 gw 192.168.7.2
-sudo route del -net 192.168.2.0/24
-sudo route add -net 192.168.2.0/24 gw 192.168.7.2
-echo "1" | sudo tee /proc/sys/net/ipv4/ip_forward
-sudo sysctl -w net.ipv4.conf.all.rp_filter=0
-sudo sysctl -w net.ipv4.conf.enx04a316b84e64.rp_filter=0
-sudo sysctl -w net.ipv4.conf.enp8s0.rp_filter=0
+#!/bin/bash
+
+sudo route add -net 192.168.6.0/24 gw 192.168.7.2 #Add USB client interface route
+sudo ifconfig enp8s0 192.168.2.10 #Configure the ethernet interface IP
+sudo route del -net 192.168.2.0/24 #Delete the default routing for the ethernet
+sudo route add -net 192.168.2.0/24 gw 192.168.7.2 #Route client computer through VLC (BBB)
+echo "1" | sudo tee /proc/sys/net/ipv4/ip_forward #Enable forwarding
+sudo sysctl -w net.ipv4.conf.all.rp_filter=0 #Disable IP filter for all interfaces
+sudo sysctl -w net.ipv4.conf.enx04a316b84e64.rp_filter=0 #Disable IP filter for USB interface
+sudo sysctl -w net.ipv4.conf.enp8s0.rp_filter=0 #Disable IP filter for ethernet interface
+sudo ufw disable #Disable Ubuntu firewall
