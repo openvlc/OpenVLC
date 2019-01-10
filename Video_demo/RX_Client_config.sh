b7e443284aeb2fbce9b07e879b451800a2648415
Diego <d22prw@gmail.com>
2019-01-10 13:06:30 +0100

    Update RX_Client_config.sh

--- Video_demo/RX_Client_config.sh
+++ Video_demo/RX_Client_config.sh
@@ -1,6 +1,8 @@
-sudo route add -net 192.168.7.0/24 gw 192.168.2.10
-echo "1" | sudo tee /proc/sys/net/ipv4/ip_forward
-sudo sysctl net.ipv4.conf.all.rp_filter=0
-sudo sysctl net.ipv4.conf.enp0s31f6.rp_filter=0
-sudo sysctl net.ipv4.conf.enx506583e15eca.rp_filter=0
-sudo ufw disable
+#!/bin/bash
+sudo route add -net 192.168.7.0/24 gw 192.168.2.10 #Route server USB interfaces through ethernet
+echo "1" | sudo tee /proc/sys/net/ipv4/ip_forward #Enable forwarding
+sudo sysctl net.ipv4.conf.all.rp_filter=0  #Disable IP filter for all interfaces
+sudo sysctl net.ipv4.conf.enp0s31f6.rp_filter=0  #Disable IP filter for USB interface
+sudo sysctl net.ipv4.conf.enx506583e15eca.rp_filter=0  #Disable IP filter for ethernet interface
+sudo ufw disable #Disable Ubuntu firewall
+
