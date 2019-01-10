142d3a6ba1f8ab0d6409e9cef1f83ff21955235d
Diego <d22prw@gmail.com>
2019-01-10 13:05:34 +0100

    Update BBB_TX_config.sh

--- Video_demo/BBB_TX_config.sh
+++ Video_demo/BBB_TX_config.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 cd kernel
-sudo ./load_test.sh
+sudo ./load_test.sh #Load OpenVLC driver
 cd ..
 cd PRU/TX
-sudo ./deploy.sh
+sudo ./deploy.sh #Load OpenVLC firmware
 cd ..
 cd ..
-echo "1" | sudo tee /proc/sys/net/ipv4/ip_forward
-sudo route add -net 192.168.6.0/24 gw 192.168.0.2
-sudo route add -net 192.168.2.0/24 gw 192.168.0.2
+echo "1" | sudo tee /proc/sys/net/ipv4/ip_forward #Enable forwarding
+sudo route add -net 192.168.6.0/24 gw 192.168.0.2 #Add USB client interface route
+sudo route add -net 192.168.2.0/24 gw 192.168.0.2 #Add ethernet client interface route
 echo "TX config"
