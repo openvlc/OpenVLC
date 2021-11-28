b627beac46dfc8ab8e741ed71df3dff4a18bf955
dayrenefrometa <68498451+dayrenefrometa@users.noreply.github.com>
2021-11-28 06:31:45 -0500

    Add files via upload

---
+++ Latest_Version/PRU/TX_PWM_Dimming_10/gen/main_pru1.pp
@@ -0,0 +1,7 @@
+gen/main_pru1.object: main_pru1.c
+gen/main_pru1.object: /usr/share/ti/cgt-pru/include/stdint.h
+gen/main_pru1.object: ../include/am335x/pru_cfg.h
+gen/main_pru1.object: resource_table_pru1.h
+gen/main_pru1.object: /usr/share/ti/cgt-pru/include/stddef.h
+gen/main_pru1.object: ../include/rsc_types.h
+gen/main_pru1.object: ../include/pru_types.h
