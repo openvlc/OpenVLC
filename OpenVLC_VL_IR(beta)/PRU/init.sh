687963b87a028a7dfd9fe7b621fc8a106878430d
dayrenefrometa <68498451+dayrenefrometa@users.noreply.github.com>
2022-12-15 17:53:36 +0100

    Delete OpenVLC_VL_IR(beta) directory

--- OpenVLC_VL_IR(beta)/PRU/init.sh
+++
@@ -1,8 +0,0 @@
-# IMDEA NETWORKS Institute: This file has been modified and it is part of the OpenVLC's source codes
-export PRU_CGT=/usr/share/ti/cgt-pru
-export PRU_SUPPORT=/opt/source/pru-software-support-package
-cd $PRU_CGT
-mkdir -p bin
-cd bin
-ln -s `which clpru`  .
-ln -s `which lnkpru` .
